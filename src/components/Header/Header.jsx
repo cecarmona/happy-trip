8377dbe76df45bb926c9a405b504c1e49b08ac54
cecarmona <cecarmona24@gmail.com>
2022-01-19 22:50:27 -0400

    Happy Trip App Completed

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+import { Autocomplete } from "@react-google-maps/api";
+import { AppBar, Toolbar, Typography, InputBase, Box } from "@material-ui/core";
+import SearchIcon from '@material-ui/icons/Search';
+import TravelIcon from "@material-ui/icons/Flight";
+
+import useStyles from './styles';
+
+const Header = ({ setCoordinates }) => {
+    const classes = useStyles();
+    const [autocomplete, setAutocomplete] = useState({});
+
+    const onLoad = (autoC) => setAutocomplete(autoC);
+
+    const onPlaceChanged = () => {
+        const lat = autocomplete.getPlace().geometry.location.lat();
+        const lng = autocomplete.getPlace().geometry.location.lng();
+
+        setCoordinates({lat, lng});
+    }
+
+    return (
+        <AppBar className={classes.menuBar} position="static">
+            <Toolbar className={classes.toolbar}>
+                <Typography variant="h5" className={classes.title}>
+                    <span><TravelIcon size="3rem"/></span>
+                    Happy Trip
+                </Typography>
+                <Box display="flex">
+                    <Typography variant="h6" className={classes.title}>
+                        Explore new experiences
+                    </Typography>
+                    <Autocomplete onLoad={onLoad} onPlaceChanged={onPlaceChanged}>
+                        <div className={classes.search}>
+                            <div className={classes.searchIcon}>
+                                <SearchIcon />
+                            </div>
+                            <InputBase placeholder="Search..." classes={{ root: classes.inputRoot, input: classes.inputInput }} />
+                        </div>
+                    </Autocomplete>
+                </Box>
+            </Toolbar>
+        </AppBar>
+    );
+}
+
+export default Header; 
